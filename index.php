770e653ca0566432ba29b602fa3fe94685d363d3
Robert Guss <robertguss@gmail.com>
2015-06-16 23:24:26 -0400

    project complete

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php 
 
 //Create Select Query
-$query = "SELECT * FROM shouts";
+$query = "SELECT * FROM shouts ORDER BY id DESC";
 $shouts = mysqli_query($con, $query);
 
 ?>
@@ -32,6 +32,11 @@ $shouts = mysqli_query($con, $query);
 				</ul>
 			</div> <!-- shouts -->
 			<div id="input">
+				<?php if(isset($_GET['error']) ) : ?>
+					<div class="error">
+						<?php echo $_GET['error']; ?>
+					</div>
+				<?php endif ?>
 				<form method="post" action="process.php">
 					<input type="text" name="user" placeholder="Enter Your Name">
 					<input type="text" name="message" placeholder="Enter a Message">
---
+++ process.php
@@ -0,0 +1,35 @@
+<?php 
+
+include 'database.php';
+
+//Check if form was submitted
+
+if(isset ($_POST['submit']) ) {
+	$user = mysqli_real_escape_string($con, $_POST['user']);
+	$message = mysqli_real_escape_string($con, $_POST['message']);
+
+	//Set timezone
+	date_default_timezone_set("America/New_York");
+	$time = date('h:i:s a', time());
+
+	// Validate Input
+	if (!isset($user) || $user == '' || !isset($message) || $message == '') {
+		$error = "Please fill in your name and message";
+		header("Location: index.php?error=".urlencode($error));
+		exit();
+	} else {
+		$query = "INSERT INTO shouts (user, message, time)
+							VALUES ('$user', '$message', '$time')";
+
+		if(!mysqli_query($con, $query)) {
+
+			die('Error: '.mysqli_error($con) );
+
+		} else {
+			header("Location: index.php");
+			exit();
+		}
+	}
+}
+
+?>
